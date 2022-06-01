632ab481806a9abef1e98408683831bbc922d12f
Cliff Chu <wave99487chu@gmail.com>
2022-06-01 15:30:03 +0800

    add jquery

---
+++ js/all.js
@@ -0,0 +1,8 @@
+$('document').ready(function() {
+  // index header scroll icon
+  $('.header-index').on('click', '#iconArrow', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $('#social').offset().top - 60 }, 300);
+  });
+
+});
